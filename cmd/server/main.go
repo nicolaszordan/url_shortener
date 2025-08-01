4b4551b556faf9d97952d4c709482e70ec952d33
Nicolas Zordan <nicolas.zordan.tek@gmail.com>
2025-08-01 16:44:11 +0200

    Refactor URL shortening routes to use correct HTTP methods

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,11 +7,17 @@ import (
 // Define the routes for the URL shortener service
 func setupRouter() *gin.Engine {
 	r := gin.Default()
-	r.GET("/shorten", func(c *gin.Context) {
+	r.POST("/shorten", func(c *gin.Context) {
 		// TODO: Handle URL shortening
 	})
-	r.GET("/expand", func(c *gin.Context) {
+	r.GET("/shorten/:shortened_url", func(c *gin.Context) {
 		// TODO: Handle URL expansion
+	})
+	r.PUT("/shorten/:shortened_url", func(c *gin.Context) {
+		// TODO: Handle URL update
+	})
+	r.DELETE("/shorten/:shortened_url", func(c *gin.Context) {
+		// TODO: Handle URL deletion
 	})
 	return r
 }
